4a3787d10a5c01cc53769bce676153b3bfc77fe0
Fernando Zurita <lzurita@infracommerce.com>
2024-05-04 13:15:07 -0600

    clone from vrp template

---
+++ routes/vrp_solver.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Request, Header, HTTPException 
+from methods.solve import solve
+
+vrp_solver = APIRouter()
+
+@vrp_solver.get('/')
+def getSchema():
+    return 
+
+    {
+        'title': 'VRP Schema', 
+        'description': 'The Vehicle Routing Problems setup',
+        '$location_start': {
+            'title': 'Start Location',
+            'description': 'The start Location',
+            'max': 1,
+            'min': 1
+        }, 
+        '$location_client': {
+            'title': 'Client Location',
+            'description': 'The start Location',
+            'demand': {
+                'title': 'Client Demand',
+                'description': 'The client demand',
+                'type': 'integer',
+                'required': True
+            }
+        }, 
+        'settings': {
+            'title': 'Settings',
+            'description': 'Settings of the problem',
+            'numberOfVehicles': {
+                'type': 'integer',
+                'description': 'Number of vehicles of the fleet',
+                'required': True
+            }
+        }
+    }
+
+@vrp_solver.post('/')
+async def resolveProblem(request: Request):
+    return await solve(request)
